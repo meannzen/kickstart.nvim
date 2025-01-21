e617030cb1e4033abb6031e3793ba81b29eb1c67
meannzen <senmeann@gmail.com>
2025-01-22 00:09:25 +0700

    change theme

--- init.lua
+++ init.lua
@@ -840,25 +840,14 @@ require('lazy').setup {
       }
     end,
   },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
+  {
+    'slugbyte/lackluster.nvim',
     lazy = false,
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-
-      -- You can configure highlights by doing something like:
-      require('github-theme').setup {}
-
-      vim.cmd.colorscheme 'github_dark'
+    priority = 1000,
+    init = function()
+      --vim.cmd.colorscheme 'lackluster'
+      vim.cmd.colorscheme 'lackluster-hack' -- my favorite
+      -- vim.cmd.colorscheme("lackluster-mint")
     end,
   },
   {
